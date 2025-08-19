57a574d6be564d98a2fb3e5a75d3a572727b5eab
Rahma Aribi <rahmaaribi35@gmail.com>
2025-08-19 21:22:17 +0100

    GetUserById & AddClient

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const userModel = require('../models/userModel');
 
+
+// Get all users
 module.exports.getAllUsers = async (req , res)=>{
 
     try {
@@ -11,3 +13,33 @@ module.exports.getAllUsers = async (req , res)=>{
     }
 
 }
+
+// Get user by ID
+module.exports.getUserById = async (req , res)=>{
+
+    try {
+        const user = await userModel.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+
+}
+
+// Add a new client
+module.exports.addClient = async (req , res)=>{
+
+    try {
+        const userData = req.body;
+        userData.role="client";
+        const user=new userModel(userData);
+        const addedUser = await user.save();
+
+        res.status(200).json(addedUser);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});    
+    }
+}
+
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,8 @@ var router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get('/getAllUsers', userController.getAllUsers);
+router.get('/getUserById/:id', userController.getUserById);
+router.post('/addClient', userController.addClient);
 
 
 module.exports = router;
