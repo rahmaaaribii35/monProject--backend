b1974916da6fcfcb7b398af636a20b13f350aff5
Rahma Aribi <rahmaaribi35@gmail.com>
2025-08-20 15:14:07 +0100

    connect product router to app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const {connectToMangoDb} = require('./config/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRouter');
+var productRouter = require('./routes/productRouter');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
