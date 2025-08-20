aad0f2c0f788fc6ed9e6cc377c5367eae64adf61
Rahma Aribi <rahmaaribi35@gmail.com>
2025-08-20 20:09:26 +0100

    addCategory & deleteCategory & updateCategory

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -10,4 +10,46 @@ module.exports.getAllCategories = async (req, res) => {
   }
 };
 
+//add category
+module.exports.addCategory = async (req, res) => {
+  try {
+    const newCategory = new categoryModel(req.body);
+    const savedCategory = await newCategory.save();
+    res.status(201).json(savedCategory);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
+//delete category
+module.exports.deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCategory = await categoryModel.findByIdAndDelete(id);
+    if (!deletedCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    res.status(200).json({ message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+//update category
+module.exports.updateCategory = async (req, res) => {
+  try {
+    const updatedCategory = await categoryModel.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        { new: true }
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -7,4 +7,10 @@ const categoryController = require("../controllers/categoryController");
 
 router.get('/getAllCategories', categoryController.getAllCategories);
 
+router.post('/addCategory', categoryController.addCategory);
+
+router.delete('/deleteCategory/:id', categoryController.deleteCategory);
+
+router.put('/updateCategory/:id', categoryController.updateCategory);
+
 module.exports = router;
