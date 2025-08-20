dc82c0450be0ed6ab3deddd7a17a84de849a757b
Rahma Aribi <rahmaaribi35@gmail.com>
2025-08-20 19:11:46 +0100

    link category to app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const {connectToMangoDb} = require('./config/db');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRouter');
 var productRouter = require('./routes/productRouter');
+var categoryRouter = require('./routes/categoryRouter');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productRouter);
+app.use('/categories', categoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
