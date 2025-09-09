42944685822b65a33a3e3ccf31323756754413c0
Rahma Aribi <rahmaaribi35@gmail.com>
2025-09-09 18:27:08 +0100

    auth in routerUser

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,25 +3,26 @@ var router = express.Router();
 const uploadImageUser = require('../middlewares/uploadImageUser');
 const isAdmin = require('../middlewares/isAdmin');
 const userController = require("../controllers/userController");
+const auth = require("../middlewares/authMiddlewares");
 
 /* GET users listing. */
 
-router.get('/getAllUsers', userController.getAllUsers);
-router.get('/getUserById/:id', userController.getUserById);
-router.get('/getUserByAge/:age', userController.getUserByAge);
-router.get('/getUserStoredByFirstName', userController.getUserStoredByFirstName);
-router.get('/getUsersByCity/:city', userController.getUsersByCity);
+router.get('/getAllUsers',auth, userController.getAllUsers);
+router.get('/getUserById/:id',auth, userController.getUserById);
+router.get('/getUserByAge/:age',auth, userController.getUserByAge);
+router.get('/getUserStoredByFirstName',auth, userController.getUserStoredByFirstName);
+router.get('/getUsersByCity/:city',auth, userController.getUsersByCity);
 
-router.get('/searchUsersByFirstName', userController.searchUsersByFirstName);
+router.get('/searchUsersByFirstName',auth, userController.searchUsersByFirstName);
 
 router.post('/addClient', userController.addClient);
 router.post('/addClientWithImage', uploadImageUser.single('user_image'), userController.addClientWithImage);
 router.post('/createAdmin', userController.createAdmin);
 
 
-router.delete('/deleteUserById/:id', userController.deleteUserById);
+router.delete('/deleteUserById/:id',auth, userController.deleteUserById);
 
-router.put('/updateUserById/:id', uploadImageUser.single('user_image'), userController.updateUserById);
+router.put('/updateUserById/:id',auth, uploadImageUser.single('user_image'), userController.updateUserById);
 
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
